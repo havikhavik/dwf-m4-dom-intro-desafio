0e56db6b05159b9e3caeb2e76041391f5340d779
Lucas Pascual <pascual_lucas117@hotmail.com>
2021-08-01 18:03:59 -0300

    ejercicio resuelto

--- index.js
+++ index.js
@@ -25,6 +25,31 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  function vaciarLista() {
+    const ulEl = document.querySelector(".lista");
+    const ulLength = ulEl.children.length;
+
+    for (let i = 0; i < ulLength; i++) {
+      ulEl.removeChild(ulEl.children[0]);
+    }
+  }
+
+  vaciarLista();
+
+  function crearNuevaLista() {
+    const ulEl = document.querySelector(".lista");
+    const listaLn = cosasQueAprendimos.length;
+
+    for (let index = 0; index < listaLn; index++) {
+      const newLi = document.createElement("li");
+      newLi.classList = cosasQueAprendimos[index].class;
+      newLi.innerText = cosasQueAprendimos[index].tema;
+      ulEl.appendChild(newLi);
+    }
+  }
+
+  crearNuevaLista();
+}
 
 main();
